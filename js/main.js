ccdc7381f973665c03c123efa7ed36a0a3e437d4
init1-dev <init1.dev@gmail.com>
2024-02-20 22:58:34 +0100

    add scroll to top button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
+function returnToTop() {
+    // Espera 200 milisegundos antes de volver al principio de la página con una animación suave
+    setTimeout(function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, 200);
+}
+
+function cambiarImagen() {
+    const imagen = document.getElementById('pc-image');
+    if (window.innerWidth > 1000) { // Cambiar el valor según la condición deseada
+        imagen.src = './assets/img/pcshape.png';
+    } else {
+        imagen.src = './assets/img/pcshape2.png';
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
+    
+    // CONTROL DEL MENU MOVIL
     const menuToggle = document.getElementById("header__menu-toggle");
     const menu = document.querySelector(".header__nav-list");
 
@@ -17,20 +38,29 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    function cambiarImagen() {
-        const imagen = document.getElementById('pc-image');
-        if (window.innerWidth > 1000) { // Cambiar el valor según la condición deseada
-            imagen.src = './assets/img/pcshape.png';
-        } else {
-            imagen.src = './assets/img/pcshape2.png';
-        }
-    }
+    // PERCENTAGE SCROLL
 
     window.addEventListener("scroll", function() {
         let scrollPosition = window.scrollY;
         let totalHeight = document.body.scrollHeight - window.innerHeight;
         let percentage = (scrollPosition / totalHeight) * 100;
         document.getElementById("percentageScroller").style.width = percentage + "%";
+    });
+
+    // BOTON TO TOP
+    window.addEventListener('scroll', function() {
+        let returnToTopButton = document.getElementById('returnToTopButton');
+        if (window.scrollY > 300) {
+            returnToTopButton.style.display = 'block';
+        } else {
+            returnToTopButton.style.display = 'none';
+        }
+    });
+
+    const toTopButton = document.getElementById('returnToTopButton');
+
+    toTopButton.addEventListener('click', function() {
+        returnToTop();
     });
 
     window.addEventListener('resize', cambiarImagen);
