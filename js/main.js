a435d6930ece3bfb6d22168fa7e296fece2c0e88
init1-dev <init1.dev@gmail.com>
2024-02-20 22:30:53 +0100

    add percentage scroll bar

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
-const menuToggle = document.getElementById("header__menu-toggle");
-const menu = document.querySelector(".header__nav-list");
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("header__menu-toggle");
+    const menu = document.querySelector(".header__nav-list");
 
-// Ocultar el menú móvil cuando se hace clic en un enlace
-menu.querySelectorAll("a").forEach(function(link) {
-    link.addEventListener("click", function() {
-        menuToggle.checked = false;
+    // Ocultar el menú móvil cuando se hace clic en un enlace
+    menu.querySelectorAll("a").forEach(function(link) {
+        link.addEventListener("click", function() {
+            menuToggle.checked = false;
+        });
     });
-});
 
-// Ocultar el menú móvil cuando se hace clic fuera del menú
-document.addEventListener("click", function(event) {
-    const target = event.target;
-    if (!target.closest(".header__nav") && menuToggle.checked) {
-        menuToggle.checked = false;
+    // Ocultar el menú móvil cuando se hace clic fuera del menú
+    document.addEventListener("click", function(event) {
+        const target = event.target;
+        if (!target.closest(".header__nav") && menuToggle.checked) {
+            menuToggle.checked = false;
+        }
+    });
+
+    function cambiarImagen() {
+        const imagen = document.getElementById('pc-image');
+        if (window.innerWidth > 1000) { // Cambiar el valor según la condición deseada
+            imagen.src = './assets/img/pcshape.png';
+        } else {
+            imagen.src = './assets/img/pcshape2.png';
+        }
     }
+
+    window.addEventListener("scroll", function() {
+        let scrollPosition = window.scrollY;
+        let totalHeight = document.body.scrollHeight - window.innerHeight;
+        let percentage = (scrollPosition / totalHeight) * 100;
+        document.getElementById("percentageScroller").style.width = percentage + "%";
+    });
+
+    window.addEventListener('resize', cambiarImagen);
+    window.addEventListener('load', cambiarImagen);
 });
-
-function cambiarImagen() {
-    const imagen = document.getElementById('pc-image');
-    if (window.innerWidth > 1000) { // Cambiar el valor según la condición deseada
-        imagen.src = './assets/img/pcshape.png';
-    } else {
-        imagen.src = './assets/img/pcshape2.png';
-    }
-}
-
-window.addEventListener('resize', cambiarImagen);
-window.addEventListener('load', cambiarImagen);
