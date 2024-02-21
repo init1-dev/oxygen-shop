7f996621b3c1ba104c24fb8b1089e94ba82ae5c4
init1-dev <init1.dev@gmail.com>
2024-02-21 17:11:00 +0100

    add contact form validation and submit message

--- js/main.js
+++ js/main.js
@@ -1,37 +1,64 @@
-function returnToTop() {
-    // Espera 200 milisegundos antes de volver al principio de la página con una animación suave
-    setTimeout(function() {
+// CONFIG
+const MEDIA_QUERY = 1000;
+const TO_TOP_SCROLL_RANGE = 500;
+const TO_TOP_WAIT_MS = 200;
+const MSG_TIMEOUT = 3000;
+// CONTACT FORM
+const CONTACT_FORM_ID = 'contact__form';
+const CONTACT_FORM_NAME_ID = 'form-name';
+const CONTACT_FORM_EMAIL_ID = 'form-email';
+const CONTACT_FORM_CHECKBOX_ID = 'form__checkbox';
+const CUSTOM_CHECKBOX_ID = 'custom-checkbox';
+const MSG_ID = 'form__submit-msg';
+
+// FUNCIONES
+const returnToTop = () => {
+    setTimeout(() => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
-    }, 200);
+    }, TO_TOP_WAIT_MS);
 }
 
-function cambiarImagen() {
+const cambiarImagen = () => {
     const imagen = document.getElementById('pc-image');
-    if (window.innerWidth > 1000) { // Cambiar el valor según la condición deseada
-        imagen.src = './assets/img/pcshape.png';
-    } else {
-        imagen.src = './assets/img/pcshape2.png';
-    }
+    imagen.src = (window.innerWidth > MEDIA_QUERY)
+        ? './assets/img/pcshape.png' 
+        : './assets/img/pcshape2.png';
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+const contactFormReset = () => {
+    document.getElementById(CONTACT_FORM_NAME_ID).style.borderColor = '#95989A';
+    document.getElementById(CONTACT_FORM_EMAIL_ID).style.borderColor = '#95989A';
+    document.getElementById(CUSTOM_CHECKBOX_ID).style.borderColor = '#95989A';
+    document.getElementById(CONTACT_FORM_ID).reset();
+}
+
+const successMsg = () => {
+    const msgSpan = document.getElementById(MSG_ID)
+    msgSpan.style.opacity = 1;
+      setTimeout(function() {
+        msgSpan.style.opacity = 0;
+      }, MSG_TIMEOUT);
+}
+
+// LISTENERS
+document.addEventListener("DOMContentLoaded", () => {
     
     // CONTROL DEL MENU MOVIL
     const menuToggle = document.getElementById("header__menu-toggle");
     const menu = document.querySelector(".header__nav-list");
 
-    // Ocultar el menú móvil cuando se hace clic en un enlace
-    menu.querySelectorAll("a").forEach(function(link) {
+    // OCULTAR MENU MOVIL CUANDO SE HACE CLICK EN UN ENLACE
+    menu.querySelectorAll("a").forEach((link) => {
         link.addEventListener("click", function() {
             menuToggle.checked = false;
         });
     });
 
-    // Ocultar el menú móvil cuando se hace clic fuera del menú
-    document.addEventListener("click", function(event) {
+    // OCULTAR MENU MOVIL CUANDO SE HACE CLICK FUERA DEL MENU
+    document.addEventListener("click", (event) => {
         const target = event.target;
         if (!target.closest(".header__nav") && menuToggle.checked) {
             menuToggle.checked = false;
@@ -39,8 +66,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // PERCENTAGE SCROLL
-
-    window.addEventListener("scroll", function() {
+    window.addEventListener("scroll", () => {
         let scrollPosition = window.scrollY;
         let totalHeight = document.body.scrollHeight - window.innerHeight;
         let percentage = (scrollPosition / totalHeight) * 100;
@@ -48,21 +74,92 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // BOTON TO TOP
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', () => {
         let returnToTopButton = document.getElementById('returnToTopButton');
-        if (window.scrollY > 500) {
-            returnToTopButton.style.display = 'block';
-        } else {
-            returnToTopButton.style.display = 'none';
-        }
+        returnToTopButton.style.display = (window.scrollY > TO_TOP_SCROLL_RANGE) ? 'block' : 'none';
     });
 
     const toTopButton = document.getElementById('returnToTopButton');
 
-    toTopButton.addEventListener('click', function() {
-        returnToTop();
-    });
+    toTopButton.addEventListener('click', returnToTop);
 
+    // CAMBIO DE IMAGEN
     window.addEventListener('resize', cambiarImagen);
     window.addEventListener('load', cambiarImagen);
+
+    // VALIDACION DEL FORMULARIO
+    document.getElementById(CONTACT_FORM_ID).addEventListener('submit', (event) => {
+        event.preventDefault();
+    
+        const NAME_VALUE = document.getElementById(CONTACT_FORM_NAME_ID).value.trim();
+        const EMAIL_VALUE = document.getElementById(CONTACT_FORM_EMAIL_ID).value.trim();
+        const IS_CHECKED = document.getElementById(CONTACT_FORM_CHECKBOX_ID).checked;
+
+        let nameOk = false;
+        let emailOk = false;
+        let isChecked = false;
+    
+        // Validamos nombre
+        let nameInput = document.getElementById(CONTACT_FORM_NAME_ID);
+        if (NAME_VALUE.length < 2 || NAME_VALUE.length > 100) {
+            nameInput.style.borderColor = 'red';
+            nameOk = false;
+        } else {
+            nameInput.style.borderColor = '#20cd17';
+            nameOk = true;
+        }
+    
+        // Validamos email
+        let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        let emailInput = document.getElementById(CONTACT_FORM_EMAIL_ID);
+        if (!emailRegex.test(EMAIL_VALUE)) {
+            emailInput.style.borderColor = 'red';
+            emailOk = false;
+        } else {
+            emailInput.style.borderColor = '#20cd17';
+            emailOk = true;
+        }
+
+        // Validamos checkbox
+        let checkbox = document.getElementById(CUSTOM_CHECKBOX_ID);
+        if (!IS_CHECKED) {
+            checkbox.style.borderColor = 'red';
+            isChecked = false;
+        } else {
+            checkbox.style.borderColor = '#20cd17';
+            isChecked = true;
+        }
+
+        if(nameOk && emailOk && isChecked) {
+            let nombre = document.getElementById(CONTACT_FORM_NAME_ID).value;
+            let email = document.getElementById(CONTACT_FORM_EMAIL_ID).value;
+
+            contactFormReset();
+            successMsg();
+
+            const formData = {
+                'name': nombre,
+                'email': email
+            }
+
+            fetch('https://jsonplaceholder.typicode.com/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            .then( resp => resp.json() )
+            .then( data => {
+                console.log('Respuesta: ', data);
+            })
+            .catch( err => {
+                console.error('Error al enviar los datos: ', err);
+            })
+
+        } else {
+            console.log("Error en formulario");
+        }
+    });
+    
 });
