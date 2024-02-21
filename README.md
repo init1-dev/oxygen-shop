7f3097d5b58577428f3de79e47920505fe9606fa
init1-dev <init1.dev@gmail.com>
2024-02-21 21:13:17 +0100

    currencies fetch api url fixed, added TODO to README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# TODO
+
+- LISTENERS:
+
+Estaría bien terminar de implementar un listener que, del mismo modo que se hace cuando la option del select cambia, teniendo el valor de la divisa por defecto establecido, en el evento DOMContentLoaded, actualice también los cambios de los respectivos precios.
--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ const fetchUrl = (url, formData) => {
 }
 
 const fetchCurrencies = () => {
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${DEFAULT_CURRENCY}.json`)
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json`)
     .then((response) => response.json())
     .then((data) => {
         currencies = {
